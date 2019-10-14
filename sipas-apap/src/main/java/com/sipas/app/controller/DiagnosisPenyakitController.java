8f3c44366ea3488cfbf2ac3115873214408abd10
diptags <rd.pradipta@gmail.com>
2019-10-14 17:28:27 +0700

    [Chores] Add basic routing in controller

--- sipas-apap/src/main/java/com/sipas/app/controller/DiagnosisPenyakitController.java
+++ sipas-apap/src/main/java/com/sipas/app/controller/DiagnosisPenyakitController.java
@@ -4,6 +4,10 @@ import com.sipas.app.service.DiagnosisPenyakitService;
 import com.sipas.app.service.PasienService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DiagnosisPenyakitController {
@@ -13,4 +17,29 @@ public class DiagnosisPenyakitController {
 
     @Autowired
     private DiagnosisPenyakitService diagnosisPenyakitService;
+
+    // Menampilkan seluruh diagnosis penyakit
+    @GetMapping(value = "/diagnosis-penyakit-all")
+    public String showAllDiagnosisPenyakit() {
+        return "diagnosispenyakit-all";
+    }
+
+    // Menampilkan diagnosis penyakit berdasarkan id diagnosis
+    @GetMapping(value = "/diagnosis-penyakit")
+    public String showDiagnosisPenyakitById(@RequestParam(value = "idDiagnosis") Long id, Model model) {
+        return "diagnosispenyakit-detail";
+    }
+
+    // Membuka form untuk menambahkan diagnosis penyakit dari pasien
+    @PostMapping(value = "/diagnosis-penyakit/tambah")
+    public String showAddDiagnosisPenyakitForm() {
+        return "diagnosispenyakit-add";
+    }
+
+    // Melakukan penghapusan data diagnosis penyakit
+    @PostMapping(value = "/diagnosis-penyakit/hapus")
+    public String deleteDiagnosisPenyakit() {
+        return "diagnosispenyakit-delete";
+    }
+
 }
--- sipas-apap/src/main/java/com/sipas/app/controller/PasienController.java
+++ sipas-apap/src/main/java/com/sipas/app/controller/PasienController.java
@@ -4,7 +4,8 @@ import com.sipas.app.service.DiagnosisPenyakitService;
 import com.sipas.app.service.PasienService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PasienController {
@@ -15,8 +16,51 @@ public class PasienController {
     @Autowired
     private DiagnosisPenyakitService diagnosisPenyakitService;
 
+    // Membuka halaman utama SIPAS
     @GetMapping("/")
-    public String home(){
-        return "beranda";
+    public String showHomePage(){
+        return "homepage";
     }
+
+    // Membuka form untuk menambahkan pasien
+    @PostMapping(value = "/pasien/tambah")
+    public String showAddPasienForm() {
+        return "pasien-add";
+    }
+
+    // Membuka detail dari pasien berdasarkan NIK pasien
+    @GetMapping(value = "/pasien")
+    public String showPasienInfoByNik(@RequestParam(value = "nikPasien") String nik, Model model) {
+        return "pasien-detail";
+    }
+
+    // Membuka form untuk mengubah pasien
+    @PostMapping(value = "/pasien/ubah/{nikPasien}")
+    public String showChangePasienForm() {
+        return "pasien-change";
+    }
+
+    // Membuka form untuk menambah diagnosis penyakit pasien
+    @PostMapping(value = "/pasien/{nikPasien}/tambah-diagnosis")
+    public String showAddDiagnosisPenyakitForm() {
+        return "pasien-add-diagnosis";
+    }
+
+    // Mencari pasien berdasarkan Asuransi dan/atau diagnosis penyakit
+    @GetMapping(value = "/pasien/cari")
+    public String showPasienInfoByAsuransiDiagnosis(
+            @RequestParam(value = "idAsuransi") Long idAsuransi,
+            @RequestParam(value = "idDiagnosis") Long idDiagnosis,
+            Model model) {
+        return "pasien-find-asuransi-diagnosis";
+    }
+
+    // Menampilkan jumlah pasien laki - laki dan perempuan di suatu diagnosis
+    @GetMapping(value = "/pasien/cari/termuda-tertua")
+    public String showPasienInfoByGenderDiagnosis(
+            @RequestParam(value = "idAsuransi") Long idAsuransi,
+            Model model) {
+        return "pasien-find-gender-diagnosis";
+    }
+
 }
