ed7c06b52f9171b4f6d667ea8d80a73f02282962
diptags <rd.pradipta@gmail.com>
2019-10-16 13:00:17 +0700

    [Chores] Ad Feature Car Pasien Berdasarkan Asuransi/Diagnosis

--- sipas-apap/src/main/java/com/sipas/app/controller/PasienController.java
+++ sipas-apap/src/main/java/com/sipas/app/controller/PasienController.java
@@ -1,21 +1,31 @@
 package com.sipas.app.controller;
 
+import com.sipas.app.model.AsuransiModel;
 import com.sipas.app.model.DiagnosisPenyakitModel;
 import com.sipas.app.model.PasienModel;
+import com.sipas.app.other.HandlingAsuransiDiagnosisSearch;
+import com.sipas.app.service.AsuransiService;
 import com.sipas.app.service.DiagnosisPenyakitService;
 import com.sipas.app.service.PasienService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.expression.Sets;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 public class PasienController {
 
     @Autowired
     private PasienService pasienService;
+
+    @Autowired
+    private AsuransiService asuransiService;
 
     @Autowired
     private DiagnosisPenyakitService diagnosisPenyakitService;
@@ -115,24 +125,64 @@ public class PasienController {
         return "pasien-find-gender-diagnosis-result";
     }
 
-//    // Mencari pasien berdasarkan Asuransi dan/atau diagnosis penyakit
-//    @GetMapping(value = "/pasien/cari")
-//    public String showPasienInfoByAsuransiDiagnosis(
-//            @RequestParam(value = "idAsuransi") Long idAsuransi,
-//            @RequestParam(value = "idDiagnosis") Long idDiagnosis,
-//            Model model) {
-//        return "pasien-find-asuransi-diagnosis";
-//    }
-
     // Membuka halaman pencarian pasien berdasarkan Asuransi dan/atau diagnosis penyakit
+    @GetMapping(value = "/pasien/cari/asuransi-diagnosis")
+    public String showPasienInfoByAsuransiDiagnosisForm(@ModelAttribute("searchHandler") HandlingAsuransiDiagnosisSearch searchHandler,
+                                                        Model model)
+    {
+        List<DiagnosisPenyakitModel> diagnosisPenyakitList = diagnosisPenyakitService.getDiagnosisPenyakitList();
+        List<AsuransiModel> asuransiList = asuransiService.getAsuransiList();
 
+        model.addAttribute("diagnosisPenyakitList", diagnosisPenyakitList);
+        model.addAttribute("asuransiList", asuransiList);
+        model.addAttribute("searchHandler", new HandlingAsuransiDiagnosisSearch());
+        return "pasien-find-asuransi-diagnosis";
+    }
 
-//    // Menampilkan jumlah pasien laki - laki dan perempuan di suatu diagnosis
-//    @GetMapping(value = "/pasien/cari/termuda-tertua")
-//    public String showPasienInfoByGenderDiagnosis(
-//            @RequestParam(value = "idAsuransi") Long idAsuransi,
-//            Model model) {
-//        return "pasien-find-gender-diagnosis";
-//    }
+    // Submit form pencarian pasien berdasarkan Asuransi dan/atau diagnosis penyakit
+    @PostMapping(value = "/pasien/cari/asuransi-diagnosis")
+    public String submitPasienInfoByAsuransiDiagnosisForm(@ModelAttribute("searchHandler") HandlingAsuransiDiagnosisSearch searchHandler,
+                                                          Model model)
+    {
+        Long idDiagnosisPenyakit = searchHandler.getIdDiagnosisPenyakit();
+        Long idAsuransi = searchHandler.getIdAsuransi();
 
+        DiagnosisPenyakitModel diagnosisPenyakit = diagnosisPenyakitService.getDiagnosisPenyakitByIdDiagnosisPenyakit(idDiagnosisPenyakit);
+        AsuransiModel asuransi = asuransiService.getAsuransiByIdAsuransi(idAsuransi);
+
+        if(idDiagnosisPenyakit.equals((long)0) || idAsuransi.equals((long)0)){
+            if(idDiagnosisPenyakit.equals((long)0) && idAsuransi.equals((long)0)){
+                model.addAttribute("searchSuccess", 0);
+            }
+            else if(idDiagnosisPenyakit.equals((long)0)){
+                List<PasienModel> listPemilik = asuransi.getListPemilik();
+                model.addAttribute("dataPencarian", listPemilik);
+                model.addAttribute("searchSuccess", 1);
+            }
+            else{
+                List<PasienModel> listPenderita = diagnosisPenyakit.getListPenderita();
+                model.addAttribute("dataPencarian", listPenderita);
+                model.addAttribute("searchSuccess", 1);
+            }
+        }
+        else{
+            List<PasienModel> listPemilik = asuransi.getListPemilik();
+            List<PasienModel> listPenderita = diagnosisPenyakit.getListPenderita();
+
+            System.out.println(listPemilik);
+            System.out.println(listPenderita);
+
+            listPemilik.retainAll(listPenderita);
+
+            if(listPemilik.size() == 0){
+                model.addAttribute("searchSuccess", 0);
+            }
+            else{
+                System.out.println(listPemilik);
+                model.addAttribute("dataPencarian", listPemilik);
+                model.addAttribute("searchSuccess", 1);
+            }
+        }
+        return "pasien-find-asuransi-diagnosis-result";
+    }
 }
